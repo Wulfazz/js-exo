d2e3d49542737df178b68f0b488ac730f8e32161
Wulfazz <teito.klein@laposte.net>
2023-11-13 16:25:34 +0100

    js exo

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// let nom = "Man"
+// let prenom = prompt ("Entrer un prénom")
+// let prenom = "Slender"
+// let age = parseInt(prompt("Entrer un âge"))
+// let age = 1000
+// let taille = 3
+// let result;
+
+// age = age + 2;
+// if (age >= 18){
+//     result = "Je suis majeur"
+// }
+// else{
+//     result = "Je ne suis pas majeur"
+// }
+
+// let carte = `${nom} ${prenom} ${age} ${result} ${taille}`
+// console.log(carte)
+
+// console.log(nom,prenom,age,taille)
+
+// exercice des moyennes : 
+
+// let m = 15.4
+// let n = 9.2
+// let l = 16.1
+// let x = 12.6
+
+// moyenne = (m+n+l+x) / 4
+
+// console.log(moyenne)
+
+// exo aléatoire
+
+let joker = Math.floor(Math.random() * 100) + 1;
+console.log(joker)
